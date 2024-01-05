306da48272f4c399c09d942976c993070fe3e89e
Sai-Samurai <78086793+Sai-Samurai@users.noreply.github.com>
2024-01-05 20:16:24 +0100

    Add files via upload

---
+++ TM_Menu.py
@@ -0,0 +1,52 @@
+import pygame
+import pygame.gfxdraw
+
+
+def draw_blur(screen):
+    screen.fill((0, 0, 0))
+    snapshot = pygame.Surface(screen.get_size())
+    snapshot.blit(screen, (0, 0))
+    snapshot.set_alpha(200)
+    snapshot = pygame.transform.smoothscale(snapshot, (int(snapshot.get_width() / 10),
+                                                       (int(snapshot.get_height() / 10))))
+    snapshot = pygame.transform.smoothscale(snapshot, (int(snapshot.get_width() * 10),
+                                                       (int(snapshot.get_height() * 10))))
+    screen.blit(snapshot, (0, 0))
+
+
+def draw_menu(screen, options, selected_option):
+    for i, option in enumerate(options):
+        text_color = (255, 255, 255) if i == selected_option else (100, 100, 100)
+        text_surface = pygame.font.Font("Retro_Gaming.ttf", 36).render(option, True, text_color)
+        menu_title_text = pygame.font.Font("Retro_Gaming.ttf", 42).render("Menu", False,
+                                                                          (255, 255, 255))
+        screen.blit(text_surface, (50, 150 + i * 50))
+        screen.blit(menu_title_text, menu_title_text.get_rect(center=(1088 / 2, 50)))
+
+
+def handle_menu(screen, menu_open, selected_option, options):
+    menu_open = True
+    while menu_open:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_UP:
+                    selected_option = (selected_option - 1) % len(options)
+
+                elif event.key == pygame.K_DOWN:
+                    selected_option = (selected_option + 1) % len(options)
+
+                    '''
+                    elif event.key == pygame.K_RETURN:
+                        if selected == 0:
+                            adjust_volume(True)
+                    '''
+                elif event.key == pygame.K_m:
+                    menu_open = False
+
+        draw_blur(screen)
+        draw_menu(screen, options, selected_option)
+        pygame.display.flip()
+    return selected_option
