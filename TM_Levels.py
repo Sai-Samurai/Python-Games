2edd5b3c5490adc311a88fdea2515cc75f672539
Sai-Samurai <78086793+Sai-Samurai@users.noreply.github.com>
2024-01-14 10:54:56 +0100

    Optimization of levels in this new file
    Optimization of the levels almost complete but with few issues:
    - Coins not appearing and not adding score +1
    
    
    What to do next:
    - Bring the text to the bottom for better interaction and either be able to make a transparent black rectangle or use PhotoShop.
    - Make a level like where the player, blocks and door get smaller.
    - Optimize the coins and add them in other levels.
    - Add music
    - Complete the menu
    - Make the text better for reading

---
+++ TM_Levels.py
@@ -0,0 +1,103 @@
+import pygame
+import TM_Texts
+import TM_Classes
+
+
+# Level definitions
+def level_list(player, door0, door1, door2, door3, door4, door5, door6, coin_list, dungeon_screen, background, lava,
+               block_1_level_1, block_2_level_1, block_3_level_1, block_4_level_1, block_5_level_1, block_6_level_1,
+               block_7_level_1, block_8_level_1, block_9_level_1,
+               block_1_level_2, block_2_level_2,
+               block_1_level_3, block_2_level_3, block_3_level_3, block_4_level_3, block_5_level_3,
+               block_1_level_4, block_2_level_4, block_3_level_4, block_4_level_4, block_5_level_4, block_6_level_4,
+               block_7_level_4, block_8_level_4, block_9_level_4, block_10_level_4, block_11_level_4, block_12_level_4,
+               block_13_level_4,
+               enemy1, enemy2, bullet1, bullet2, enemy3, enemy4, bullet3, bullet4):
+    levels = [
+        {"background": background, "sprites": [player], "doors": [door0], "texts": [0]},
+
+        {"background": dungeon_screen, "sprites": [player], "doors": [door1],
+         "blocks": [block_1_level_1, block_2_level_1, block_3_level_1, block_4_level_1, block_5_level_1,
+                    block_6_level_1, block_7_level_1, block_8_level_1, block_9_level_1], "texts": [1],
+         "coin_list": coin_list},
+
+        {"background": dungeon_screen, "sprites": [player], "doors": [door2],
+         "blocks": [block_1_level_2, block_2_level_2],
+         "lava": [lava], "texts": [2], "coin_list": coin_list},
+
+        {"background": dungeon_screen, "sprites": [player], "doors": [door3],
+         "blocks": [block_1_level_3, block_2_level_3, block_3_level_3, block_4_level_3, block_5_level_3],
+         "enemies": [enemy1, enemy2], "bullets": [bullet1, bullet2], "texts": [3]},
+
+        {"background": dungeon_screen, "sprites": [player], "doors": [door4],
+         "blocks": [block_1_level_4, block_2_level_4, block_3_level_4, block_4_level_4, block_5_level_4,
+                    block_6_level_4, block_7_level_4, block_8_level_4, block_9_level_4, block_10_level_4,
+                    block_11_level_4, block_12_level_4, block_13_level_4], "enemies": [enemy3, enemy4],
+         "bullets": [bullet3, bullet4], "texts": [4]},
+
+        {"background": dungeon_screen, "sprites": [player], "doors": [door5], "texts": [5]},
+
+        {"background": background, "sprites": [player], "doors": [door6], "texts": [6]}
+    ]
+
+    return levels
+
+
+def game_levels(screen, exit_sprite, blocks_sprite, enemy_group, bullet_group, lava_group, current_level,
+                level_text, score_text, lives_text):
+    # Common functionalities for each level
+    screen.blit(current_level["background"], (0, 0))
+
+    for sprite in current_level["sprites"]:
+        sprite.appear(screen)
+
+    if "doors" in current_level:
+        exit_sprite.empty()
+        for door in current_level["doors"]:
+            exit_sprite.add(door)
+            door.appear(screen)
+    else:
+        exit_sprite.empty()
+
+    if "blocks" in current_level:
+        blocks_sprite.empty()
+        for block in current_level["blocks"]:
+            blocks_sprite.add(block)
+            block.appear(screen)
+    else:
+        blocks_sprite.empty()
+
+    if "enemies" in current_level:
+        enemy_group.empty()
+        for enemy in current_level["enemies"]:
+            enemy_group.add(enemy)
+            enemy.appear(screen)
+    else:
+        enemy_group.empty()
+
+    if "bullets" in current_level:
+        bullet_group.empty()
+        for bullet in current_level["bullets"]:
+            bullet_group.add(bullet)
+            bullet.animate()
+            bullet.appear(screen)
+    else:
+        bullet_group.empty()
+
+    if "lava" in current_level:
+        lava_group.empty()
+        for lava in current_level["lava"]:
+            lava_group.add(lava)
+            lava.appear(screen)
+    else:
+        lava_group.empty()
+
+    for text_index in current_level["texts"]:
+        TM_Texts.display_text(text_index)
+
+    if 0 < current_level["texts"][0] < 7:
+        screen.blit(level_text, (750, 20))
+        screen.blit(score_text, (450, 20))
+        screen.blit(lives_text, (150, 20))
+
+    pygame.display.flip()
